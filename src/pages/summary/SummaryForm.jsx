101b7ff673e5df784c4550e252e24d88eb948cf4
Richu <richujain1995@gmail.com>
2022-09-20 02:38:15 -0400

    Commiting with failed tests

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,11 +1,35 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { OverlayTrigger, Popover } from "react-bootstrap";
 export default function SummaryForm() {
-  const [tcChecked, setTcChecked] = useState(false);
+  // const popover = (
+  //     <Popover id="popover-basic">
+  //       <Popover.Header as="h3">Popover right</Popover.Header>
+  //       <Popover.Body>
+  //         And here's some <strong>amazing</strong> content. It's very engaging.
+  //         right?
+  //       </Popover.Body>
+  //     </Popover>
+  //   );
 
+  //   const Example = () => (
+  //     <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+  //       <Button variant="success">Click me to see</Button>
+  //     </OverlayTrigger>
+  //   );
+  const [tcChecked, setTcChecked] = useState(false);
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Header as="h3">Popover right</Popover.Header>
+      <Popover.Body>no ice cream will actually be delivered</Popover.Body>
+    </Popover>
+  );
   const checkboxLabel = (
     <span>
+      <OverlayTrigger placement="right" overlay={popover}>
+        <Button variant="success">Click me to see</Button>
+      </OverlayTrigger>
       I agree to <span style={{ color: "blue" }}> Terms and Conditions</span>
     </span>
   );
--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -3,8 +3,16 @@
 // 2. Checking checkbox enables button
 // 3. Unchecking checkbox again disables button
 
-import { fireEvent, render, screen } from "@testing-library/react";
+//Install UserEvent
+// npm install --save-dev @testing-library/user-event @testing-library/dom
+
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
 
 test("Checkbox is unchecked by default", () => {
   render(<SummaryForm />);
@@ -19,21 +27,63 @@ test("Checkbox is unchecked by default", () => {
   });
   expect(confirmOrderButton).toBeDisabled();
 });
-test("Checking and unchecking checkbox enables and disabled button", () => {
+test("Checkbox enables button on first click and disables on second click", async () => {
+  const user = userEvent.setup();
   render(<SummaryForm />);
-
   const checkbox = screen.getByRole("checkbox", {
     name: /terms and conditions/i,
   });
-  const confirmOrderButton = screen.getByRole("button", {
-    name: /confirm order/i,
-  });
+  const confirmButton = screen.getByRole("button", { name: /confirm order/i });
 
-  // Checking checkbox enables button
-  fireEvent.click(checkbox);
-  expect(confirmOrderButton).toBeEnabled();
+  await user.click(checkbox);
+  expect(confirmButton).toBeEnabled();
 
-  // Unchecking checkbox disabled button
-  fireEvent.click(checkbox);
-  expect(confirmOrderButton).toBeDisabled();
+  await user.click(checkbox);
+  expect(confirmButton).toBeDisabled();
 });
+test("popover responds to hover", async () => {
+  render(<SummaryForm />);
+
+  // popover starts out hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  // popover appears upon mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+
+  await userEvent.hover(termsAndConditions);
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument(); // ******Not necessary because the getBy in above line throws an error and this won't be executed. But adding this line is the best practise and for readability.
+
+  // popover disappears when we mouse out
+  await userEvent.unhover(termsAndConditions);
+  //   const nullPopoverAgain = screen.queryByText(
+  //     /no ice cream will actually be delivered/i
+  //   );
+  // expect(nullPopoverAgain).not.toBeInTheDocument();
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText(/no ice cream will actually be delivered/i)
+  );
+});
+
+// Doesn't work on Verison 14. Only works on Version 13.
+// test("Checking and unchecking checkbox enables and disabled button", () => {
+//   render(<SummaryForm />);
+
+//   const checkbox = screen.getByRole("checkbox", {
+//     name: /terms and conditions/i,
+//   });
+//   const confirmOrderButton = screen.getByRole("button", {
+//     name: /confirm order/i,
+//   });
+
+//   // Checking checkbox enables button
+//   userEvent.click(checkbox);
+//   expect(confirmOrderButton).toBeEnabled();
+
+//   // Unchecking checkbox disabled button
+//   userEvent.click(checkbox);
+//   expect(confirmOrderButton).toBeDisabled();
+// });
